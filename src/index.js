d0601dd2b0e39f1d37f747bdbdba5df203267058
Thiago Kaefer <thiagothiago.kaefer@gmail.com>
2020-10-19 17:25:29 -0300

    Correcao da edicao

--- src/index.js
+++ src/index.js
@@ -5,20 +5,38 @@ var app = new Vue({
         desc: '',
         date: '',
         agenda: [],
+        index: -1,
     },
     methods: {
         salvar() {
-            this.agenda.push({
-                title: this.title,
-                desc: this.desc,
-                date: this.date,
-            })
+            if (this.index == -1) {
+
+                this.agenda.push({
+                    title: this.title,
+                    desc: this.desc,
+                    date: this.date,
+                })
+            } else {
+                Object.assign(this.agenda[this.index], {
+                    title: this.title,
+                    desc: this.desc,
+                    date: this.date,
+                })
+                this.index = -1
+            }
+            this.title = ''
+            this.desc = ''
+            this.date = ''
         },
         apagar(item) {
             let select_usuario = this.agenda.indexOf(item)
             this.agenda.splice(select_usuario, 1)
         },
-        editar() {
+        editar(item) {
+            this.index = this.agenda.indexOf(item)
+            this.title = item.title
+            this.desc = item.desc
+            this.date = item.date
 
         }
     }
