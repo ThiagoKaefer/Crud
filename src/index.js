816e4b2b09ae6860b0525f50bc63b8f2161bfb4a
Thiago Kaefer <thiagothiago.kaefer@gmail.com>
2020-10-20 14:07:09 -0300

    Validação

--- src/index.js
+++ src/index.js
@@ -8,8 +8,36 @@ var app = new Vue({
         index: -1,
     },
     methods: {
-        salvar() {
-            if (this.index == -1) {
+        validate(){
+            var x = document.forms["myForm"]["fname"].value;
+                if (x == "") {
+                    alert("Name must be filled out");
+                return false;
+                }
+        },  
+        salvar() { 
+            if(this.title == '' && this.desc == '' && this.date == '' ){
+                alert("Todos os campos devem estar preenchidos");
+
+            } else if(this.title == '' && this.desc == ''){
+                alert("O Título e a Descrição devem estar preenchidos");
+
+            }else if(this.desc == '' && this.date == ''){
+                alert("Descrição e Data devem estar preenchidos");
+
+            }else if(this.title == '' && this.date == ''){
+                alert("O Título e a Data devem estar preenchidos");
+
+            }else if(this.title == ''){
+                alert("O Título deve estar preenchido");
+
+            }else if(this.date == '' ){
+                alert("A Data deve estar preenchida");
+
+            }else if(this.desc == ''){
+                alert("A Descrição deve estar preenchida");
+
+            }else if (this.index == -1) {
 
                 this.agenda.push({
                     title: this.title,
